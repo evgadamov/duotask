743917644499913aa51b4ea0389249d6c54caad4
Ruslan Magamadov <ruslanmarlinskiy@gmail.com>
2025-12-17 22:00:29 +0300

    Initial Commit

---
+++ DuoTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DuoTask
+//
+//  Created by Ruslan Marlinsky on 17.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

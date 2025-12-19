87e19050ee5a8bead9ac5e25b64df778e3c70daf
Ruslan Magamadov <ruslanmarlinskiy@gmail.com>
2025-12-19 21:57:43 +0300

    Добавил таблицу, создал модельку таски, добавил моковые данные

--- DuoTask/ViewController.swift
+++ DuoTask/ViewController.swift
@@ -8,13 +8,66 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private enum Constants {
+        static let idCell = "taskCell"
+    }
+    
+    var taskStorage: [Task] = []
+    
+    let tableview: UITableView = {
+        let tv = UITableView()
+        tv.backgroundColor = .clear
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        return tv
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        setupTableView()
+        setupMockData()
+        setupStorage()
     }
+    
+    func setupTableView() {
+        view.addSubview(tableview)
+        
+        NSLayoutConstraint.activate([
+            tableview.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableview.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableview.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableview.leftAnchor.constraint(equalTo: view.leftAnchor)
+        ])
+    }
+    
+    func setupStorage() {
+        tableview.register(UITableViewCell.self, forCellReuseIdentifier: Constants.idCell)
+        tableview.dataSource = self
+    }
+    
+    func setupMockData() {
+        taskStorage = [
+            Task(id: UUID(), subject: "task 1", body: "hello"),
+            Task(id: UUID(), subject: "task 2", body: "bye"),
+            Task(id: UUID(), subject: "task 3", body: "hi")
+        ]
+    }
+}
 
-
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        taskStorage.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.idCell, for: indexPath)
+        let item = taskStorage[indexPath.row]
+        var config = cell.defaultContentConfiguration()
+        config.text = item.subject
+        cell.contentConfiguration = config
+        return cell
+    }
 }
 
